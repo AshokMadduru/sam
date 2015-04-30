cbc6d79317e0b06c6d6989baf2bf5986cff46224
Ashok Madudru <ashokgirihari@gmail.com>
2015-04-30 16:31:23 +0530

    geturl class and getall users classes are added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import cgi
 import urllib
 
@@ -9,6 +10,7 @@ import datetime
 import jinja2
 import os
 import json
+import datetime
 
 # Setting up the Jinja environment to include html pages as templates
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -122,10 +124,18 @@ HTML = """\
 class GetMail(webapp2.RequestHandler):
     def post(self):
         mail = self.request.get("email")
-        chromedata = self.request.get('browser_name',"chrome_data")
-        qry = Chrome.query(ancestor = browser_key(chromedata))
-        data_query = qry.filter(Chrome.email == mail)
-        dataa = data_query.fetch()
+##        chromedata = self.request.get('browser_name',"chrome_data")
+##        qry = Chrome.query(ancestor = browser_key(chromedata))
+##        data_query = qry.filter(Chrome.email == mail)
+##        dataa = data_query.fetch()
+
+        d = str(datetime.date.today())
+        dt = d[-2:]+'/'+d[5:7]+'/'+d[:4]+' 00:00:00'
+
+        qry = "SELECT * FROM Chrome WHERE email='"+mail+"' AND timeStamp>'"+dt+"'"## ORDER BY timeStamp DESC"
+        data_query = ndb.gql(qry)
+        data = data_query.fetch()
+            
         global result
         result = []
         global HTML
@@ -139,7 +149,7 @@ class GetMail(webapp2.RequestHandler):
         """
         global count
         count = 0
-        for row in dataa:
+        for row in data:
             temp = {}
             temp['evenType'] = row.eventType
             temp['urlLink'] = row.urlLink
@@ -149,7 +159,7 @@ class GetMail(webapp2.RequestHandler):
             result.append(temp)
             count = count + 1
             HTML = HTML+"<tr><td>"+row.timeStamp+"</td><td>"+row.eventType+"</td><td>"+row.urlLink+"</td><td>"+row.datas+"</td></tr>"
-            if count > 1000:
+            if count > 2000:
                 break
         HTML = HTML+"</table></div></body></html>"
         chrome_data= {'data':result}
@@ -157,17 +167,57 @@ class GetMail(webapp2.RequestHandler):
         #self.response.write(mail)
 class GetUsers(webapp2.RequestHandler):
     def post(self):
-        chromedata = self.request.get('browser_name',"chrome_data")
-        qry = Chrome.query(ancestor = browser_key(chromedata))
-        #data_query = qry.filter(Chrome.email == mail)
-        dataa = qry.fetch()
-        global resu
-        resu = []
-        for row in dataa:
-            if row.email not in resu and '@' in row.email:
-                resu.append(row.email)
-        user_list = {'users':resu}
-        self.response.write(json.dumps(user_list))
+        try:
+            chromedata = self.request.get('browser_name',"chrome_data")
+ ##           qry = Chrome.query(ancestor = browser_key(chromedata))
+##            data_query = qry.filter(projection = ['email'], distinct = True)
+##            dataa = qry.fetch()
+            qry = "SELECT DISTINCT email FROM Chrome"
+            data_query = ndb.gql(qry)
+            data = data_query.fetch()
+            global resu
+            resu = []
+            for row in data:
+                if row.email not in resu and '@' in row.email:
+                    resu.append(row.email)
+            user_list = {'users':resu}
+            self.response.write(json.dumps(user_list))
+        except Exception , e:
+            self.response.write(str(e))
+class DashBoard(webapp2.RequestHandler):
+    def get(self):
+        global html
+        html = """\
+        <html>
+          <body border=\"1\" style=\"width:100%\"><h1>List of Students</h1>
+            <div>
+            <table>
+            <tr><th>Sl No</th><th>Student Email</th></tr>
+        
+        """
+        try:
+            qry = "SELECT DISTINCT email FROM Chrome"
+            data_query = ndb.gql(qry)
+            data = data_query.fetch()
+            global resu
+            resu = []
+            count = 1
+            for row in data:
+                if row.email not in resu and '@' in row.email:
+                    resu.append(row.email)
+                    html = html + "<tr><td>"+str(count)+"</td><td>"+row.email+"</td></tr>"
+                    count = count+1
+            user_list = {'users':resu}
+            html = html+"</table></div></body></html>"
+            self.response.write(html)
+        except Exception ,e:
+            self.response.write(str(e))
+      
+##    def student_data():
+##        qry = "SELECT DISTINCT urllink from Chrome WHERE email = "
+##        data_query = ndb.gql(qry)
+##        data = data_query.fetch()
+        
 class GetUserData(webapp2.RequestHandler):
     def post(self):
         mail = self.request.get("email")
@@ -233,7 +283,7 @@ class Meta(ndb.Model):
 ## class for storing metadata
 class StoreMeta(webapp2.RequestHandler):
     def post(self):
-        chap = "2"
+        chap = "3"
         uRl = self.request.get("url")
         tiTle = self.request.get("title")
         tyPe = self.request.get("type")
@@ -265,7 +315,89 @@ class Student(ndb.Model):
     name = ndb.StringProperty(indexed = True)
     uRl = ndb.StructuredProperty(Url)
 ## storing student data
+class GetAllUrls(webapp2.RequestHandler):
+    def get(self):
+        try:
+            qry = "SELECT DISTINCT url FROM Meta"
+            data_query = ndb.gql(qry)
+            data = data_query.fetch()
+##            users_qry = "SELECT DISTINCT email FROM Chrome"
+##            users_data_query = ndb.gql(users_qry)
+##            users_data = users_data_query.fetch()
+##            for uRl in data:
+##                for user in users_data:
+##                    stu_qry = "SELECT DISTINCT timeStamp FROM Chrome WHERE email='"+user.email+"' AND urlLink='"+uRl.url+"'"
+##                    stu_data_query = ndb.gql(stu_qry)
+##                    stu_data = stu_data_query.fetch()
+            global htmll
+            htmll = """\
+            <html>
+              <body border=\"1\" style=\"width:100%\"><h1>URL Access History</h1>
+                <div>
+                <table>
+                <tr><th>Sl No</th><th>URL</th><th>Count</th></tr>
+        
+            """
+            global c
+            c = 1
+            for uRl in data:
+                link = uRl.url
+                count_qry = "SELECT timeStamp FROM Chrome WHERE urlLink='"+link+"'"
+                count_query = ndb.gql(count_qry)
+                count_data = count_query.fetch()
+                global count
+                count = 0
+                for row in count_data:
+                    count = count+1
+                htmll = htmll+"<tr><td>"+c+"</td><td>"+link+"</td><td>"+count+"</td></tr>"
+                c = c+1
+####            
+####            count = 1
+####            for row in data:
+####                if row.email not in resu and '@' in row.email:
+####                    resu.append(row.email)
+####                    html = html + "<tr><td>"+str(count)+"</td><td>"+row.email+"</td></tr>"
+####                    count = count+1
+####            user_list = {'users':resu}
+            html = html+"</table></div></body></html>"
+            self.response.write(html)
+        except Exception ,e:
+            self.response.write(str(e))
 
+## Get Url Data
+class GetUrlData(webapp2.RequestHandler):
+    def get(self):
+        chromedata = self.request.get('browser_name',"chrome_data")
+        qry = Chrome.query(ancestor = browser_key(chromedata))
+        #data_query = qry.filter(Chrome.email == "anveshika.09@gmail.com")
+        dataa = qry.fetch()
+        result = {}
+        count = 0
+        for row in dataa:
+            count = count + 1
+            if count > 1000:
+                break
+            if (result.has_key(row.urlLink)):
+                result[row.urlLink][0] = result[row.urlLink][0] + 1
+                result[row.urlLink][1].append(row.email)
+                result[row.urlLink][1] = list(set(result[row.urlLink][1]))
+            else:
+                result[row.urlLink] = [1,[row.email]]
+        global HTML
+        HTML = """\
+        <html>
+          <body border=\"1\" style=\"width:100%\">
+            <div>
+            <table>
+            <tr><th>URL</th><th>Count</th><th>Students</th></tr>
+        
+        """
+        for url in result:        
+            HTML = HTML+"<tr><td>"+url+"</td><td>"+str(result[url][0])+"</td><td>"+''.join(result[url][1])+"</td</tr>"
+        HTML = HTML+"</table></div></body></html>"
+        chrome_data= {'data':result}
+        self.response.write(HTML)
+        #self.response.write(mail)
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/chrome',ChromeData),
@@ -277,5 +409,7 @@ app = webapp2.WSGIApplication([
     ('/getuserdata',GetUserData),
     ('/savemetadata',MetaData),
     ('/storemeta',StoreMeta),
-    #('/getstudentdata',setStudentData),
+    ('/dashboard',DashBoard),
+    ('/geturldata',GetUrlData),
+    ('/geturls',GetAllUrls),
 ], debug=True)
--- sample.py
+++ sample.py
@@ -4,8 +4,10 @@ from httplib import HTTPConnection
 import urllib
 import json
 
-url = "http://student-monitor.appspot.com/getuserdata"
-data = urllib.urlencode({"email":"dasarianvesh100@gmail.com"})
-resp = urllib.urlopen(url,data)
-#result = json.loads(resp.read())
-print(resp.read())
+##url = "http://student-monitor.appspot.com/getusers"
+##data = urllib.urlencode({})
+##resp = urllib.urlopen(url,data)
+##result = json.loads(resp.read())
+d = str(datetime.date.today())
+dt = d[-2:]+'/'+d[5:7]+'/'+d[:4]+' 00:00:00'
+print(str(type(dt)))
